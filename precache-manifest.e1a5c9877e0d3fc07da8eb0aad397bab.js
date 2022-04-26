99290c05bbf5baf17271d22531897a8750f84e32
zkh <573385407@qq.com>
2022-04-26 09:54:09 +0800

    deploy

---
+++ precache-manifest.e1a5c9877e0d3fc07da8eb0aad397bab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab147ce755e09d84be0",
+    "url": "/morney-website/css/app.83ef2f8b.css"
+  },
+  {
+    "revision": "67077c233726cbd14bb7a8cebfe1a2c0",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "0ab147ce755e09d84be0",
+    "url": "/morney-website/js/app.78a23e8b.js"
+  },
+  {
+    "revision": "9befaee18d47e19f83e2",
+    "url": "/morney-website/js/chunk-vendors.60c9969d.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "0ab762f9fbb926b603a06c707b81b351",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
